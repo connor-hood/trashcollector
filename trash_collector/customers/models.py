7e64d14e95e83f219645bf0aafec10bf51d5aab0
Connor Hood <connor_hood@yahoo.com>
2021-05-07 11:16:02 -0500

    started on customer edit page

--- trash_collector/customers/models.py
+++ trash_collector/customers/models.py
@@ -1,3 +1,5 @@
+import datetime
+
 from django.db import models
 # Create your models here.
 
@@ -7,3 +9,7 @@ from django.db import models
 class Customer(models.Model):
     name = models.CharField(max_length=50)
     user = models.ForeignKey('accounts.User', blank=True, null=True, on_delete=models.CASCADE)
+    zipcode = models.IntegerField(max_length=5)
+    collect_day = models.DateField
+    special_day = models.DateField(datetime.datetime)
+    balance = models.IntegerField(max_length=3)
