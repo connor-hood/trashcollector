9afc99928d5698b718c6a3b767a68fdad46d99b4
DAVID paredes <augustspies22@gmail.com>
2021-05-11 13:26:35 -0700

    Fixed the issue with displaying to index for employees now on to details of employee

--- trash_collector/employees/models.py
+++ trash_collector/employees/models.py
@@ -9,5 +9,5 @@ from django.db import models
 class Employee(models.Model):
     emp_name = models.CharField(max_length=50)
     user = models.ForeignKey('accounts.User', blank=True, null=True, on_delete=models.CASCADE)
-    zipcode = models.IntegerField(max_length=5, default=None)
+    zipcode = models.IntegerField(default=None)
 
